9ef030ae64afb26ecc38e71929f02bc4613c1cb9
AlexChau <alex.chau@finbourne.com>
2024-09-07 23:42:12 +0100

    Start writing Parser, adds AST node interfaces and classes

---
+++ Monkey.Ast/Ast.cs
@@ -0,0 +1,61 @@
+﻿using Monkey.Interpreter.Lexing;
+
+namespace Monkey.Ast;
+public interface INode
+{
+    string TokenLiteral();
+}
+
+public interface IStatement : INode
+{
+    void StatementNode();
+}
+
+public interface IExpression : INode
+{
+    void ExpressionNode();
+}
+
+public class Program : INode
+{
+    public IStatement[] Statements;
+
+    public Program(IStatement[] statements)
+    {
+        Statements = statements;
+    }
+
+    public string TokenLiteral()
+    {
+        if (Statements.Length > 0)
+        {
+            return Statements[0].TokenLiteral();
+        }
+        else
+        {
+            return "";
+        }
+    }
+}
+
+public class LetStatement : IStatement
+{
+    Token Token;
+    Identifier Name;
+    IExpression Value;
+
+    public void StatementNode() { }
+    public string TokenLiteral() { return Token.Literal; }
+}
+
+public class Identifier : IExpression
+{
+    Token Token;
+    string Value;
+
+    public void ExpressionNode() { }
+    public string TokenLiteral() { return Token.Literal; }
+}
+
+
+
---
+++ Monkey.Parser/Parser.cs
@@ -0,0 +1,24 @@
+﻿using Monkey.Interpreter.Lexing;
+using Monkey.Ast;
+namespace Monkey.Parser;
+public class Parser {
+    Lexer _lexer;
+    Token curToken;
+    Token peekToken;
+
+    public Parser(Lexer l) {
+        _lexer = l;
+        // Call NextToken twice to populate curToken and peekToken fields
+        NextToken();
+        NextToken();
+    }
+
+    private void NextToken() {
+        curToken = peekToken;
+        peekToken = _lexer.NextToken();
+    }
+
+    public Program ParseProgram() {
+        return null; // Remove this
+    }
+}
