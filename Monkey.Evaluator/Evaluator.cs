470ed2efa59ba2252f2bea97b0d9a53f73e62236
AxterDoesCode <chau.alex88@hotmail.com>
2024-10-12 14:54:53 +0100

    We are evaluating MORE

--- Monkey.Evaluator/Evaluator.cs
+++ Monkey.Evaluator/Evaluator.cs
@@ -1,28 +1,207 @@
-﻿using Monkey.Ast;
+﻿using System.Dynamic;
+using LanguageExt;
+using Monkey.Ast;
+using Monkey.Object;
 
 namespace Monkey.Evaluator;
 public class Evaluator
 {
-    public Object.IObject Eval(Ast.INode node) {
+    public readonly Object.Boolean TRUE = new(true);
+    public readonly Object.Boolean FALSE = new(false);
+    public Null NULL = new() { };
+
+    public IObject Eval(INode node)
+    {
         switch (node)
         {
-            case Ast.Program p:
-                return EvalStatements(p.Statements.ToArray());
-            case Ast.ExpressionStatement e:
+            case Program p:
+                return EvalProgram(p.Statements.ToArray());
+            case ExpressionStatement e:
                 return Eval(e.Expression);
-            case Ast.IntegerLiteral x:
+            case IntegerLiteral x:
                 return new Object.Integer(x.Value);
-            // case Ast.Boolean x:
-            //     return new Object.Boolean(x.Value);
+            case Ast.Boolean x:
+                return NativeBoolToBooleanObject(x.Value);
+            case PrefixExpression x:
+                var right = Eval(x.Right);
+                if (IsError(right))
+                {
+                    return right;
+                }
+                return EvalPrefixExpression(x.Operator, right);
+            case InfixExpression x:
+                var left = Eval(x.Left);
+                if (IsError(left))
+                {
+                    return left;
+                }
+                var right2 = Eval(x.Right);
+                if (IsError(right2))
+                {
+                    return right2;
+                }
+                return EvalInfixExpression(x.Operator, left, right2);
+            case IfExpression x:
+                return EvalIfExpression(x);
+            case BlockStatement x:
+                return EvalBlockStatement(x.Statements.ToArray());
+            case ReturnStatement x:
+                var val = Eval(x.ReturnValue);
+                return IsError(val) ? val : new ReturnValue(val);
         }
-        return null;
+        return NULL;
     }
 
-    public Object.IObject EvalStatements(IStatement[] statements) {
-        Object.IObject result = new Object.Integer(1);
-        foreach (var stmt in statements) {
+    private bool IsError(IObject obj)
+    {
+        if (!obj.IsNull())
+        {
+            return obj.Type() == ObjectType.ERROR_OBJ;
+        }
+        return false;
+    }
+
+    private IObject EvalIfExpression(IfExpression ie)
+    {
+        var condition = Eval(ie.Condition);
+        if (IsError(condition))
+        {
+            return condition;
+        }
+        if (IsTruthy(condition))
+        {
+            return Eval(ie.Consequence);
+        }
+        else
+        {
+            return ie.Alternative.Match(
+                None: NULL,
+                Some: x => Eval(x)
+            );
+        }
+    }
+
+    private bool IsTruthy(IObject obj)
+    {
+        return obj switch
+        {
+            Object.Boolean x => x.value,
+            Object.Null => false,
+            _ => false,
+        };
+    }
+
+    private IObject EvalInfixExpression(string op, IObject l, IObject r)
+    {
+        if (l.Type() == ObjectType.INTEGER_OBJ && r.Type() == ObjectType.INTEGER_OBJ)
+        {
+            return EvalIntegerInfixExpression(op, l, r);
+        }
+        else if (op == "==")
+        {
+            return NativeBoolToBooleanObject(l == r);
+        }
+        else if (op == "!=")
+        {
+            return NativeBoolToBooleanObject(l != r);
+        }
+        else if (l.Type() != r.Type())
+        {
+            return NewError("type mismatch: {0} {1} {2}", l.Type(), op, r.Type());
+        }
+        else
+        {
+            return NewError("unknown operator: {0} {1} {2}", l.Type(), op, r.Type());
+        }
+    }
+
+    private IObject EvalIntegerInfixExpression(string op, IObject l, IObject r)
+    {
+        var leftVal = ((Integer)l).value;
+        var rightVal = ((Integer)r).value;
+        return op switch
+        {
+            "+" => new Integer(leftVal + rightVal),
+            "-" => new Integer(leftVal - rightVal),
+            "*" => new Integer(leftVal * rightVal),
+            "/" => new Integer(leftVal / rightVal),
+            "<" => NativeBoolToBooleanObject(leftVal < rightVal),
+            ">" => NativeBoolToBooleanObject(leftVal > rightVal),
+            "==" => NativeBoolToBooleanObject(leftVal == rightVal),
+            "!=" => NativeBoolToBooleanObject(leftVal != rightVal),
+            _ => NewError("unknown operator: {0} {1} {2}", l.Type(), op, r.Type()),
+        };
+    }
+
+    private IObject EvalPrefixExpression(string op, IObject right)
+    {
+        return op switch
+        {
+            "!" => EvalBangOperator(right),
+            "-" => EvalMinusOperator(right),
+            _ => NewError("unknown operator: {0}{1}", op, right.Type()),
+        };
+    }
+
+    private IObject EvalMinusOperator(IObject right)
+    {
+        if (right.Type() != ObjectType.INTEGER_OBJ)
+        {
+            return NewError("unknown operator: -{0}", right.Type());
+        }
+        var value = ((Integer)right).value;
+        return new Integer(-value);
+    }
+
+    private IObject EvalBangOperator(IObject right)
+    {
+        return right switch
+        {
+            Object.Boolean x => x.value ? FALSE : TRUE,
+            Object.Null => TRUE,
+            _ => FALSE,
+        };
+    }
+
+    public IObject EvalProgram(IStatement[] statements)
+    {
+        IObject result = NULL;
+        foreach (var stmt in statements)
+        {
             result = Eval(stmt);
+
+            switch (result)
+            {
+                case ReturnValue r:
+                    return r.Value;
+                case Error e:
+                    return e;
+            }
         }
         return result;
+    }
+
+    private IObject EvalBlockStatement(IStatement[] statements)
+    {
+        IObject result = NULL;
+        foreach (var stmt in statements)
+        {
+            result = Eval(stmt);
+            if (result != null && (result.Type() == ObjectType.RETURN_VALUE_OBJ || result.Type() == ObjectType.ERROR_OBJ))
+            {
+                return result;
+            }
+        }
+        return result;
+    }
+
+    private IObject NativeBoolToBooleanObject(bool input)
+    {
+        return input ? TRUE : FALSE;
+    }
+
+    private Object.Error NewError(string format, params string[] args)
+    {
+        return new Object.Error(String.Format(format, args));
     }
 }
--- Monkey.Object/Object.cs
+++ Monkey.Object/Object.cs
@@ -10,11 +10,33 @@ public class ObjectType
     public const string INTEGER_OBJ = "INTEGER";
     public const string BOOLEAN_OBJ = "BOOLEAN";
     public const string NULL_OBJ = "NULL";
+    public const string RETURN_VALUE_OBJ = "RETURN_VALUE";
+    public const string ERROR_OBJ = "ERROR";
 }
 
+public class Error : IObject
+{
+    public Error(string errMsg) {
+        Message = errMsg;
+    }
+    public string Message;
+    public string Type() => ObjectType.ERROR_OBJ;
+    public string Inspect() => $"Error: {Message}";
+}
+
+public class ReturnValue : IObject
+{
+    public ReturnValue(IObject obj) {
+        Value = obj;
+    }
+    public IObject Value;
+    public string Type() { return ObjectType.RETURN_VALUE_OBJ; }
+    public string Inspect() { return Value.Inspect(); }
+}
 public class Integer : IObject
 {
-    public Integer (long val) {
+    public Integer(long val)
+    {
         value = val;
     }
     public long value;
@@ -24,7 +46,8 @@ public class Integer : IObject
 
 public class Boolean : IObject
 {
-    public Boolean (bool b) {
+    public Boolean(bool b)
+    {
         value = b;
     }
     public bool value;
